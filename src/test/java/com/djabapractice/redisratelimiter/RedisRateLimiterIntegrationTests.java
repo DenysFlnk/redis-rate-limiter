bb76459fcbec74fd5f4920f32aeb5b11b1b7bc17
DimaGalchenko <galchenkodima31@gmail.com>
2025-01-19 23:14:43 +0200

    Refactor tests

--- src/test/java/com/djabapractice/redisratelimiter/RedisRateLimiterIntegrationTests.java
+++ src/test/java/com/djabapractice/redisratelimiter/RedisRateLimiterIntegrationTests.java
@@ -26,7 +26,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Testcontainers
 @SpringBootTest
-@TestMethodOrder(MethodOrderer.MethodName.class)
 class RedisRateLimiterIntegrationTests {
 
     @Container
@@ -50,86 +49,93 @@ class RedisRateLimiterIntegrationTests {
         jedisPool = new JedisPool(redisContainer.getHost(), redisContainer.getMappedPort(6379));
     }
 
-    @Test
-    @DisplayName("Fixed Window Rate Limiter: Should allow 5 requests in 1 minute window")
-    void testFixedWindowRateLimiter_withinLimit_shouldAllow() {
-        FixedWindowRateLimiter rateLimiter = new FixedWindowRateLimiter(jedisPool, 5, 1);
-        String userId = UUID.randomUUID().toString();
-        for (int i = 0; i < 5; i++) {
-            assertDoesNotThrow(() -> rateLimiter.isAllowed(userId));
+    @Nested
+    @DisplayName("Main Tests:")
+    @TestMethodOrder(MethodOrderer.MethodName.class)
+    class MainTests {
+        @Test
+        @DisplayName("Fixed Window Rate Limiter: Should allow 5 requests in 1 minute window")
+        void testFixedWindowRateLimiter_withinLimit_shouldAllow() {
+            FixedWindowRateLimiter rateLimiter = new FixedWindowRateLimiter(jedisPool, 5, 1);
+            String userId = UUID.randomUUID().toString();
+            for (int i = 0; i < 5; i++) {
+                assertDoesNotThrow(() -> rateLimiter.isAllowed(userId));
+            }
+        }
+
+        @Test
+        @DisplayName("Fixed Window Rate Limiter: Should throw exception after exceeding limit")
+        void testFixedWindowRateLimiter_exceedLimit_shouldThrowException() {
+            FixedWindowRateLimiter rateLimiter = new FixedWindowRateLimiter(jedisPool, 5, 1);
+
+            String userId = UUID.randomUUID().toString();
+
+            for (int i = 0; i < 5; i++) {
+                assertDoesNotThrow(() -> rateLimiter.isAllowed(userId));
+            }
+
+            assertThrows(RateLimiterExceededException.class, () -> rateLimiter.isAllowed(userId));
+        }
+
+        @Test
+        @DisplayName("Sliding Window Rate Limiter: Should allow 5 requests within window")
+        void testSlidingWindowRateLimiter_withinLimit_shouldAllow() {
+            SlidingWindowRateLimiter rateLimiter = new SlidingWindowRateLimiter(jedisPool, 5, 60000);
+
+            String userId = UUID.randomUUID().toString();
+
+            for (int i = 0; i < 5; i++) {
+                assertDoesNotThrow(() -> rateLimiter.isAllowed(userId));
+            }
+        }
+
+        @Test
+        @DisplayName("Sliding Window Rate Limiter: Should throw exception after exceeding limit")
+        void testSlidingWindowRateLimiter_exceedLimit_shouldThrowException() {
+            SlidingWindowRateLimiter rateLimiter = new SlidingWindowRateLimiter(jedisPool, 5, 60000);
+
+            String userId = UUID.randomUUID().toString();
+
+            for (int i = 0; i < 5; i++) {
+                assertDoesNotThrow(() -> rateLimiter.isAllowed(userId));
+            }
+
+            assertThrows(RateLimiterExceededException.class, () -> rateLimiter.isAllowed(userId));
+        }
+
+        @Test
+        @DisplayName("Sliding Window Rate Limiter with Lua: Should allow 5 requests within window")
+        void testSlidingWindowRateLimiterWithLua_withinLimit_shouldAllow() {
+            SlidingWindowRateLimiterWithLua rateLimiter = new SlidingWindowRateLimiterWithLua(jedisPool, 5, 60000);
+
+            String userId = UUID.randomUUID().toString();
+
+            for (int i = 0; i < 5; i++) {
+                assertDoesNotThrow(() -> rateLimiter.isAllowed(userId));
+            }
+        }
+
+        @Test
+        @DisplayName("Sliding Window Rate Limiter with Lua: Should throw exception after exceeding limit")
+        void testSlidingWindowRateLimiterWithLua_exceedLimit_shouldThrowException() {
+            SlidingWindowRateLimiterWithLua rateLimiter = new SlidingWindowRateLimiterWithLua(jedisPool, 5, 60000);
+
+            String userId = UUID.randomUUID().toString();
+
+            for (int i = 0; i < 5; i++) {
+                assertDoesNotThrow(() -> rateLimiter.isAllowed(userId));
+            }
+
+            assertThrows(RateLimiterExceededException.class, () -> rateLimiter.isAllowed(userId));
         }
     }
 
-    @Test
-    @DisplayName("Fixed Window Rate Limiter: Should throw exception after exceeding limit")
-    void testFixedWindowRateLimiter_exceedLimit_shouldThrowException() {
-        FixedWindowRateLimiter rateLimiter = new FixedWindowRateLimiter(jedisPool, 5, 1);
 
-        String userId = UUID.randomUUID().toString();
-
-        for (int i = 0; i < 5; i++) {
-            assertDoesNotThrow(() -> rateLimiter.isAllowed(userId));
-        }
-
-        assertThrows(RateLimiterExceededException.class, () -> rateLimiter.isAllowed(userId));
-    }
-
-    @Test
-    @DisplayName("Sliding Window Rate Limiter: Should allow 5 requests within window")
-    void testSlidingWindowRateLimiter_withinLimit_shouldAllow() {
-        SlidingWindowRateLimiter rateLimiter = new SlidingWindowRateLimiter(jedisPool, 5, 60000);
-
-        String userId = UUID.randomUUID().toString();
-
-        for (int i = 0; i < 5; i++) {
-            assertDoesNotThrow(() -> rateLimiter.isAllowed(userId));
-        }
-    }
-
-    @Test
-    @DisplayName("Sliding Window Rate Limiter: Should throw exception after exceeding limit")
-    void testSlidingWindowRateLimiter_exceedLimit_shouldThrowException() {
-        SlidingWindowRateLimiter rateLimiter = new SlidingWindowRateLimiter(jedisPool, 5, 60000);
-
-        String userId = UUID.randomUUID().toString();
-
-        for (int i = 0; i < 5; i++) {
-            assertDoesNotThrow(() -> rateLimiter.isAllowed(userId));
-        }
-
-        assertThrows(RateLimiterExceededException.class, () -> rateLimiter.isAllowed(userId));
-    }
-
-    @Test
-    @DisplayName("Sliding Window Rate Limiter with Lua: Should allow 5 requests within window")
-    void testSlidingWindowRateLimiterWithLua_withinLimit_shouldAllow() {
-        SlidingWindowRateLimiterWithLua rateLimiter = new SlidingWindowRateLimiterWithLua(jedisPool, 5, 60000);
-
-        String userId = UUID.randomUUID().toString();
-
-        for (int i = 0; i < 5; i++) {
-            assertDoesNotThrow(() -> rateLimiter.isAllowed(userId));
-        }
-    }
-
-    @Test
-    @DisplayName("Sliding Window Rate Limiter with Lua: Should throw exception after exceeding limit")
-    void testSlidingWindowRateLimiterWithLua_exceedLimit_shouldThrowException() {
-        SlidingWindowRateLimiterWithLua rateLimiter = new SlidingWindowRateLimiterWithLua(jedisPool, 5, 60000);
-
-        String userId = UUID.randomUUID().toString();
-
-        for (int i = 0; i < 5; i++) {
-            assertDoesNotThrow(() -> rateLimiter.isAllowed(userId));
-        }
-
-        assertThrows(RateLimiterExceededException.class, () -> rateLimiter.isAllowed(userId));
-    }
 
     @Nested
     @TestMethodOrder(MethodOrderer.MethodName.class)
-    @DisplayName("[Optional tests]:")
-    class MultithreadedTests {
+    @DisplayName("[Additional tests]:")
+    class AdditionalTests {
         @Test
         @DisplayName("Sliding Window Rate Limiter [1 second sleep after invocation]: Should allow 10 requests within 5 seconds window ")
         void testSlidingWindowRateLimiter_withinLimit_shouldAllow() throws InterruptedException {
